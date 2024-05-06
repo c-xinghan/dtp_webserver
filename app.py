382efc8ff42f42ead154065117abbefb55047eba
Chua Xing Han <xinghan.740@gmail.com>
2024-05-07 03:53:43 +0800

    Replaced render_template with redirect where applicable. Removed dead code.

--- app.py
+++ app.py
@@ -1,48 +1,35 @@
-import re
 import json
 from datetime import datetime, timedelta
 
-from flask import Flask, request
-from flask import render_template
+from flask import Flask, request, redirect, render_template, session, url_for
 
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
+# from urllib.parse import urlencode
+# from urllib.request import Request, urlopen
 
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
 speed = 0.0
 time = datetime.now()
 socketio = SocketIO(app)
-
-#defines the job
-# def job():
-#     new_price = "mum";
-    #job emits on websocket
-    # socketio.emit('price update', new_price, broadcast=True)
-
-#schedule job
-# scheduler = BackgroundScheduler()
-# running_job = scheduler.add_job(job, 'interval', seconds=4, max_instances=1)
-# scheduler.start()
+# development key, replace with your own
+app.secret_key = 'dti'
 
 @app.route("/")
 def home():
     return render_template("home.html", speed=speed, time=time)
 
-
 @app.route("/speed/")
 def get_speed():
     return json.dumps(speed)
-
 
 @app.route("/updatespeed/", methods=['POST'])
 def update_speed():
     global speed     
     speed = float(request.headers.get('speed'))
-    socketio.emit('price update', speed, broadcast=True)
-    return render_template("home.html", speed=speed, time=time)
+    socketio.emit('speed update', speed, broadcast=True)
+    return redirect(url_for("home"))
 
 @app.route("/time/")
 def get_time():
@@ -52,32 +39,27 @@ def get_time():
 def decrease_time():
     global time
     time -= timedelta(hours=1)
-    return render_template("home.html", speed=speed, time=time)
-
+    return redirect(url_for("home"))
 
 @app.route("/increasetime/", methods=['POST'])
 def increase_time():
     global time
     time += timedelta(hours=1)
-    return render_template("home.html", speed=speed, time=time)
+    session['speed'], session['time'] = speed, time
+    return redirect(url_for("home"))
 
 @app.route("/decreasespeed/", methods=['POST'])
 def decrease_speed():
     global speed
     speed -= 3
-    return render_template("home.html", speed=speed, time=time)
-
+    return redirect(url_for("home"))
 
 @app.route("/increasespeed/", methods=['POST'])
 def increase_speed():
     global speed
     speed += 3
-    return render_template("home.html", speed=speed, time=time)
-
-
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
+    return redirect(url_for("home"))
+    # return render_template("home.html", speed=speed, time=time)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0')
